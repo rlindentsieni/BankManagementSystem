8c9ff552a9896bbade66ce9ab7938aa5f8189737
rlindentsieni <rilindentsieni@gmail.com>
2024-09-19 15:14:55 +0200

    First form created

--- BankManagementSystem/Form1.cs
+++ BankManagementSystem/Form1.cs
@@ -16,5 +16,10 @@ namespace BankManagementSystem
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
