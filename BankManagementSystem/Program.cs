f1560735fde08c5f24da873a7d65c70a38ab7b7a
rlindentsieni <rilindentsieni@gmail.com>
2024-09-24 21:48:05 +0200

    Code updated

--- BankManagementSystem/Program.cs
+++ BankManagementSystem/Program.cs
@@ -16,7 +16,7 @@ namespace BankManagementSystem
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Registration());
         }
     }
 }
