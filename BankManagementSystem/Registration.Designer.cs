c4d2d7575e2b5532605c479be46b0b55c2b9fef6
rlindentsieni <rilindentsieni@gmail.com>
2024-09-24 21:42:48 +0200

    Validation code added , and the structure was updated

---
+++ BankManagementSystem/Registration.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace BankManagementSystem
+{
+    partial class Registration
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnRegister = new System.Windows.Forms.Button();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtConfirmPass = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblPass = new System.Windows.Forms.Label();
+            this.lblPassMatching = new System.Windows.Forms.Label();
+            this.lblEmailVal = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(244, 86);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(265, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter your details to register";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(273, 136);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Names:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(285, 187);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Email:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(253, 242);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(88, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Password:";
+            // 
+            // btnRegister
+            // 
+            this.btnRegister.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnRegister.Enabled = false;
+            this.btnRegister.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRegister.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnRegister.Location = new System.Drawing.Point(205, 360);
+            this.btnRegister.Name = "btnRegister";
+            this.btnRegister.Size = new System.Drawing.Size(392, 48);
+            this.btnRegister.TabIndex = 6;
+            this.btnRegister.Text = "Register";
+            this.btnRegister.UseVisualStyleBackColor = false;
+            this.btnRegister.Click += new System.EventHandler(this.btnRegister_Click);
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(385, 136);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(262, 22);
+            this.txtName.TabIndex = 7;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(386, 187);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(262, 22);
+            this.txtEmail.TabIndex = 8;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(386, 236);
+            this.txtPassword.MaxLength = 5;
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '5';
+            this.txtPassword.Size = new System.Drawing.Size(262, 22);
+            this.txtPassword.TabIndex = 9;
+            this.txtPassword.UseSystemPasswordChar = true;
+            this.txtPassword.TextChanged += new System.EventHandler(this.txtPassword_TextChanged);
+            // 
+            // txtConfirmPass
+            // 
+            this.txtConfirmPass.Location = new System.Drawing.Point(386, 289);
+            this.txtConfirmPass.MaxLength = 5;
+            this.txtConfirmPass.Name = "txtConfirmPass";
+            this.txtConfirmPass.PasswordChar = '5';
+            this.txtConfirmPass.Size = new System.Drawing.Size(262, 22);
+            this.txtConfirmPass.TabIndex = 10;
+            this.txtConfirmPass.UseSystemPasswordChar = true;
+            this.txtConfirmPass.TextChanged += new System.EventHandler(this.txtConfirmPass_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(189, 291);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(152, 20);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Confirm Password:";
+            // 
+            // lblPass
+            // 
+            this.lblPass.AutoSize = true;
+            this.lblPass.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPass.ForeColor = System.Drawing.Color.Red;
+            this.lblPass.Location = new System.Drawing.Point(382, 261);
+            this.lblPass.Name = "lblPass";
+            this.lblPass.Size = new System.Drawing.Size(147, 16);
+            this.lblPass.TabIndex = 12;
+            this.lblPass.Text = "Must have 5 characters.";
+            this.lblPass.Visible = false;
+            // 
+            // lblPassMatching
+            // 
+            this.lblPassMatching.AutoSize = true;
+            this.lblPassMatching.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPassMatching.ForeColor = System.Drawing.Color.Red;
+            this.lblPassMatching.Location = new System.Drawing.Point(382, 314);
+            this.lblPassMatching.Name = "lblPassMatching";
+            this.lblPassMatching.Size = new System.Drawing.Size(145, 16);
+            this.lblPassMatching.TabIndex = 13;
+            this.lblPassMatching.Text = "Password not matching";
+            this.lblPassMatching.Visible = false;
+            // 
+            // lblEmailVal
+            // 
+            this.lblEmailVal.AutoSize = true;
+            this.lblEmailVal.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEmailVal.ForeColor = System.Drawing.Color.Red;
+            this.lblEmailVal.Location = new System.Drawing.Point(384, 212);
+            this.lblEmailVal.Name = "lblEmailVal";
+            this.lblEmailVal.Size = new System.Drawing.Size(136, 16);
+            this.lblEmailVal.TabIndex = 15;
+            this.lblEmailVal.Text = "Invalid Email address";
+            this.lblEmailVal.Visible = false;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.Red;
+            this.label7.Location = new System.Drawing.Point(383, 161);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 16);
+            this.label7.TabIndex = 14;
+            this.label7.Visible = false;
+            // 
+            // Registration
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblEmailVal);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.lblPassMatching);
+            this.Controls.Add(this.lblPass);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtConfirmPass);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.btnRegister);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Registration";
+            this.Text = " ";
+            this.Load += new System.EventHandler(this.Registration_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnRegister;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtConfirmPass;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblPass;
+        private System.Windows.Forms.Label lblPassMatching;
+        private System.Windows.Forms.Label lblEmailVal;
+        private System.Windows.Forms.Label label7;
+    }
+}
---
+++ BankManagementSystem/Registration.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BankManagementSystem
+{
+    public partial class Registration : Form
+    {
+        static bool registered = false; // static makes it usable/accessible in other forms.
+        public Registration()
+        {
+
+            InitializeComponent();
+        }
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+            Login loginForm = new Login(); //Login form object.
+            //Pass data to the login form:
+            loginForm.lblName.Text = txtName.Text;
+            loginForm.lblPass.Text = txtConfirmPass.Text;
+            loginForm.lblEmail.Text = txtEmail.Text;
+            
+            MessageBox.Show("You Have Successfully registerd!");
+            this.Hide();
+            loginForm.ShowDialog(); // show the login form.
+            this.Close();
+
+        }
+
+        private void txtPassword_TextChanged(object sender, EventArgs e)
+        {   
+            // Password length validation:
+            if (txtPassword.Text.Length < 5)
+            {
+                lblPass.Show();
+                registered = false;
+            }
+            else { 
+                lblPass.Hide();
+                registered = true;
+            }
+        }
+
+        private void txtConfirmPass_TextChanged(object sender, EventArgs e)
+        {
+            // Password confrimation validation (Matching):
+            if (txtPassword.Text != txtConfirmPass.Text)
+            {
+                lblPassMatching.Show();
+                registered = false;
+            }
+            else { 
+                lblPassMatching.Hide();
+                lblPassMatching.Text = "Password Correct!";
+                lblPassMatching.ForeColor = Color.Green;
+                lblPassMatching.Show();
+                registered = true;
+            }
+
+            // Last validation: Enable the register button if No Errors
+            if (registered) { 
+                btnRegister.Enabled = true;
+                btnRegister.BackColor = Color.Black;
+            }
+
+        }
+
+        private void Registration_Load(object sender, EventArgs e)
+        {
+            if (!btnRegister.Enabled)
+            {
+                btnRegister.BackColor = Color.White;
+            }
+        }
+    }
+}
