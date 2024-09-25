0b9c13f2d9aad0afd1fdada0531a7a792d732b9b
rlindentsieni <rilindentsieni@gmail.com>
2024-09-25 15:34:39 +0200

    Added sub heading and content

--- BankManagementSystem/AboutUs.Designer.cs
+++ BankManagementSystem/AboutUs.Designer.cs
@@ -32,6 +32,12 @@
             this.label1 = new System.Windows.Forms.Label();
             this.btnback = new System.Windows.Forms.Button();
             this.btnexit = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblmessage = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -60,9 +66,9 @@
             this.btnback.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.btnback.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnback.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.btnback.Location = new System.Drawing.Point(519, 200);
+            this.btnback.Location = new System.Drawing.Point(595, 345);
             this.btnback.Name = "btnback";
-            this.btnback.Size = new System.Drawing.Size(201, 62);
+            this.btnback.Size = new System.Drawing.Size(128, 44);
             this.btnback.TabIndex = 2;
             this.btnback.Text = "Back";
             this.btnback.UseVisualStyleBackColor = false;
@@ -73,19 +79,80 @@
             this.btnexit.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.btnexit.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnexit.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.btnexit.Location = new System.Drawing.Point(519, 295);
+            this.btnexit.Location = new System.Drawing.Point(465, 345);
             this.btnexit.Name = "btnexit";
-            this.btnexit.Size = new System.Drawing.Size(201, 62);
+            this.btnexit.Size = new System.Drawing.Size(129, 44);
             this.btnexit.TabIndex = 3;
             this.btnexit.Text = "Exit";
             this.btnexit.UseVisualStyleBackColor = false;
             this.btnexit.Click += new System.EventHandler(this.btnexit_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(221, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(421, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Welcome to living where we deliver cutting-edge technology solutions.";
+            // 
+            // lblmessage
+            // 
+            this.lblmessage.AutoSize = true;
+            this.lblmessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblmessage.Location = new System.Drawing.Point(461, 137);
+            this.lblmessage.Name = "lblmessage";
+            this.lblmessage.Size = new System.Drawing.Size(131, 20);
+            this.lblmessage.TabIndex = 5;
+            this.lblmessage.Text = "What we offer.";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(546, 171);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(108, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "- Secure banking";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(546, 199);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(87, 16);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "- 24/7 Access";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(546, 228);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(147, 16);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "- Personalized services";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(546, 254);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(119, 16);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "- Customer support";
             // 
             // AboutUs
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblmessage);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.btnexit);
             this.Controls.Add(this.btnback);
             this.Controls.Add(this.label1);
@@ -104,5 +171,11 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button btnback;
         private System.Windows.Forms.Button btnexit;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblmessage;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
     }
 }
