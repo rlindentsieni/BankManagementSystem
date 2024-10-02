c87a180b59dc71db4a5a2d868ef21d4bd72985b0
rlindentsieni <rilindentsieni@gmail.com>
2024-10-02 19:59:43 +0200

    changes made

--- BankManagementSystem/Registration.Designer.cs
+++ BankManagementSystem/Registration.Designer.cs
@@ -195,7 +195,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.BackColor = System.Drawing.Color.LightSteelBlue;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.lblEmailVal);
             this.Controls.Add(this.label7);
